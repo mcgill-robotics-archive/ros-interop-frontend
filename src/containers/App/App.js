ebac2260a088cd3cc16301735bd213f83611c5e0
Anass Al <mr@anassinator.com>
2017-06-12 00:02:56 -0700

    Add remote save/update/delete target/image functionality

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ROSClient from '../../actions/ros';
 import { Canvas, Target, Sidebar, TargetList, Notification } from '../../components';
 
 
@@ -39,6 +40,11 @@ class App extends React.Component {
       // Notification container.
       notification: undefined,
     };
+
+    // ROS Client.
+    this.client = new ROSClient(this.notify);
+    this.client.connect('ws://kinetic:9090');
+    this.remoteIDs = {};
   }
 
   componentWillMount() {
@@ -65,24 +71,37 @@ class App extends React.Component {
   };
 
   handleDeleteTarget = (index) => {
-    delete this.state.target_images[index];
-    delete this.state.targets[index];
-    this.setState({
-      targets: this.state.targets,
-      target_images: this.state.target_images,
-    }, () => this.notify(`DELETED TARGET ${index}`));
+    const remoteID = this.remoteIDs[index];
+    this.client.deleteTarget(remoteID, () => {
+      // This automatically deletes the corresponding image.
+      delete this.state.target_images[index];
+      delete this.state.targets[index];
+      delete this.remoteIDs[index];
+      this.setState({
+        targets: this.state.targets,
+        target_images: this.state.target_images,
+      }, () => this.notify(`DELETED TARGET ${index}`));
+    });
   };
 
-  handleSubmitTarget = (index) => {
+  handleSubmitTarget = (index, state) => {
     const latestIndex = this.state.new_target
       ? this.state.latest_index + 1
       : this.state.latest_index;
-    this.state.target_images[index] = this.state.preview_image;
-    this.setState({
-      new_target: false,
-      latest_index: latestIndex,
-      target_images: this.state.target_images,
-    }, () => this.notify(`SAVED TARGET ${index}`));
+    const image = this.state.preview_image;
+    const remoteID = this.remoteIDs[index];
+
+    this.client.setTarget(remoteID, state, (id) => {
+      this.remoteIDs[index] = id;
+      this.client.setTargetImage(id, image, () => {
+        this.state.target_images[index] = image;
+        this.setState({
+          new_target: false,
+          latest_index: latestIndex,
+          target_images: this.state.target_images,
+        }, () => this.notify(`SAVED TARGET ${index}`));
+      });
+    });
   };
 
   handleNewTarget = () => {
@@ -113,7 +132,7 @@ class App extends React.Component {
     const notification = (
       <Notification
         message={msg}
-        dismissAfter={500}
+        dismissAfter={1000}
         onDismiss={() => {
           this.setState({ notification: undefined });
         }}
