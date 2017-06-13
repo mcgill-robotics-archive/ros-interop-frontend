aba7c2c56ce460349896865d0d1ca0c001c93e06
Anass Al <mr@anassinator.com>
2017-06-13 02:47:57 -0700

    Add blank image on startup

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 const Sidebar = (props) => {
   const styles = require('./Sidebar.css');
 
+  const blank = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+  const img = props.preview || blank;
+
   return (
     <div>
       <div className={styles.content}>
-        <img className={styles.preview} src={props.preview} alt="preview" />
+        <img
+          className={styles.preview}
+          src={img}
+          alt="preview"
+        />
         {props.target}
         <button
           className={styles.add}
