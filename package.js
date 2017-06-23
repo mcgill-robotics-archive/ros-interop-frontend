ba1ad5cfc1cae0bd5eba70d97aa7b350b1c930df
Anass Al <mr@anassinator.com>
2017-06-22 20:16:41 -0700

    Fix release package.js

--- package.js
+++ package.js
@@ -2,26 +2,32 @@ const fs = require('fs');
 const packager = require('electron-packager');
 
 const pkg = {
-  name: 'MyApp',
-  productName: 'MyApp',
-  version: '0.0.1',
-  main: 'main.js'
+  name: 'Interop',
+  productName: 'Interop',
+  version: '0.1.0',
+  main: 'main.js',
 }
 
-fs.writeFile('./build/package.json', JSON.stringify(pkg), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      packager({
-        dir: './build',
-        arch: 'all', // ia32, x64, all
-        platform: 'all', // linux, win32, darwin, mas, all
-        'app-copyright': '',
-        'app-version': pkg.version,
-        prune: false,
-        out: './release'
-      }, function(err){
-        if (err) console.error(err);
-      });
-    }
+fs.writeFile('./build/package.json', JSON.stringify(pkg), (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    packager({
+      dir: './build',
+      arch: 'all', // ia32, x64, all
+      platform: [
+        'darwin',
+        'linux',
+      ],
+      appCopyright: 'McGill Robotics',
+      appVersion: pkg.version,
+      icon: './src/assets/images/ros.ico',
+      prune: false,
+      out: './release',
+    }, (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
 });
