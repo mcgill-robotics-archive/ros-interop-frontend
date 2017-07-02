3bae100ae245040e8c19b51f9d08d350eb232eed
Anass Al <mr@anassinator.com>
2017-07-02 14:19:00 -0700

    Only package for current architecture

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ const packager = require('electron-packager');
 
 const pkg = {
   name: 'Interop',
-  productName: 'Interop',
+  productName: 'interop_frontend',
   version: '0.1.0',
   main: 'main.js',
 }
@@ -14,15 +14,11 @@ fs.writeFile('./build/package.json', JSON.stringify(pkg), (err) => {
   } else {
     packager({
       dir: './build',
-      arch: 'all', // ia32, x64, all
-      platform: [
-        'darwin',
-        'linux',
-      ],
       appCopyright: 'McGill Robotics',
       appVersion: pkg.version,
       icon: './src/assets/images/ros.ico',
       prune: false,
+      quiet: true,
       out: './release',
     }, (err) => {
       if (err) {
